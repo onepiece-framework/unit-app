ea2e6eae70b95de1ebf5390c434d2f7ce0881dfc
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-11-29 14:12:24 +0900

    Fix: App.class.php - 2020

--- App.class.php
+++ App.class.php
@@ -2,67 +2,160 @@
 /**
  * unit-app:/App.class.php
  *
- * @creation  2018-04-04
+ * @created   2018-04-04
  * @version   1.0
  * @package   unit-app
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
 
+/** namespace
+ *
+ * @created   2019-02-20
+ */
+namespace OP\UNIT;
+
+/** Used class.
+ *
+ * @created   2019-02-20
+ */
+use Exception;
+use OP\OP_CORE;
+use OP\OP_UNIT;
+use OP\OP_SESSION;
+use OP\IF_UNIT;
+use OP\IF_APP;
+use OP\Env;
+use OP\Cookie;
+use OP\Notice;
+use OP\UNIT_APP;
+use OP\UNIT_ROUTER;
+use OP\UNIT_LAYOUT;
+use OP\UNIT_TEMPLATE;
+use function OP\RootPath;
+use function OP\ConvertURL;
+use function OP\ConvertPath;
+
 /** App
  *
- * @creation  2018-04-04
+ * @created   2018-04-04
  * @version   1.0
  * @package   unit-app
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
-class App
+class App implements IF_UNIT, IF_APP
 {
 	/** trait.
 	 *
 	 */
-	use OP_CORE, OP_SESSION;
+	use OP_CORE, OP_UNIT, OP_SESSION, UNIT_APP, UNIT_ROUTER, UNIT_LAYOUT, UNIT_TEMPLATE;
 
-	static private $_DISPATCH_	 = 'OP\UNIT\NEWWORLD\Dispatch';
-	static private $_LAYOUT_	 = 'OP\UNIT\NEWWORLD\Layout';
-	static private $_ROUTER_	 = 'OP\UNIT\NEWWORLD\Router';
-	static private $_TEMPLATE_	 = 'OP\UNIT\NEWWORLD\Template';
+	/** SmartURL Arguments.
+	 *
+	 * @var array
+	 */
+	private $_args;
 
-	/** Automatically run.
+	/** Content is the result of execute the endpoint.
+	 *
+	 * @var string
+	 */
+	private $_content;
+
+	/** ETag
 	 *
 	 */
-	static function Auto()
+	private function _ETag()
 	{
-		//	Execute end-point.
-		$content = self::$_DISPATCH_::Get();
 
-		//	The content is wrapped in the Layout.
-		echo self::$_LAYOUT_::Get($content);
 	}
 
-	/** Get SmartURL arguments.
+	/** Automatically.
 	 *
-	 * @return	 array	 $args
 	 */
-	static function Args()
+	function Auto()
 	{
-		return self::$_ROUTER_::Get()['args'];
+		try{
+			//	Is http?
+			if( Env::isHttp() ){
+
+				//	Set MIME
+				Env::Mime('text/html');
+
+				//	Get End-Point.
+				$endpoint = $this->EndPoint();
+
+				//	Get End-Point content.
+				$this->_content = self::__TEMPLATE_GET($endpoint, ['app'=>$this]);
+
+				//	ETag
+				$this->_ETag();
+
+				//	Layout
+				$this->__LAYOUT();
+
+				/*
+
+				//	Get current mime.
+				$mime = Env::Mime();
+
+				//	Check whether to do layout.
+				if( $mime === 'text/html' and Env::Get('layout')['execute'] ?? null ){
+					//	Do layout.
+					$this->Unit('Layout')->Auto($this->_content);
+				}else{
+					//	No layout.
+					echo $this->_content;
+				};
+
+				//	...
+				unset($this->_content);
+
+				*/
+
+			}else{
+				//	...
+				$root = $_SERVER['PWD'].'/';
+				$path = $_SERVER['argv'][1] ?? 'index.php';
+				$file = $root . $path;
+
+				//	...
+				if(!$endpoint = realpath($file) ){
+					throw new Exception("This file has not been exists. ($file)");
+				};
+
+				//	...
+				$this->Template($endpoint);
+			};
+		}catch( \Throwable $e ){
+			Notice::Set($e);
+		};
+	}
+
+	/** Output content.
+	 *
+	 * @created  2019-11-25
+	 */
+	function Content()
+	{
+		echo $this->_content;
 	}
 
 	/** Template
 	 *
-	 * @param	 string	 $path
-	 * @param	 string	 $args
+	 * @created  2019-11-21
+	 * @param    string      $path
+	 * @param    string      $args
+	 * @return   string      $content
 	 */
-	static function Template($path, $args=null)
+	function Template(string $path, array $args=[])
 	{
-		if( $args and !is_array($args)){
-			$type = is_object($args) ? get_class($args) : gettype($args);
-			D("Argument is not array. ($type)");
-			return;
-		}
-		self::$_TEMPLATE_::Run($path, $args);
+		//	...
+		$args['app'] = $this;
+
+		//	...
+		return $this->__TEMPLATE($path, $args, true);
 	}
 
 	/** Layout
@@ -74,74 +167,156 @@ class App
 	 * $layout = App::Layout(); // Get layout name.
 	 * </pre>
 	 *
-	 * @param	 null|boolean|string	 $value
+	 * @updated  2019-05-10  Optimized.
+	 * @param    null|boolean|string    $value
+	 * @return   null|boolean|string    $value
 	 */
-	static function Layout($name=null)
+	static function Layout($val=null)
 	{
-		//	...
-		switch( $type = gettype($name) ){
-			case 'boolean':
-				//	...
-				self::$_LAYOUT_::Execute($name);
-
-				//	...
-				if(!$name ){
-					self::$_LAYOUT_::Name('');
-				}
-				break;
-
-			case 'string':
-				//	...
-				self::$_LAYOUT_::Name($name);
-
-				//	...
-				if( $name ){
-					self::$_LAYOUT_::Execute(true);
-				}
-				break;
-
-			default:
-		}
-
-		//	...
-		return self::$_LAYOUT_::Execute() ? self::$_LAYOUT_::Name() : false;
+		return self::__LAYOUT_CONFIG($val);
 	}
 
-	/** WebPack
+	/** Register WebPack file.
 	 *
-	 * @param	 string	 $path
+	 * @param  string|array  $path
 	 */
-	static function WebPack($path)
+	function WebPack($path)
 	{
-		//	...
-		if(!class_exists('OP\UNIT\WebPack') ){
-			if(!Unit::Load('webpack') ){
-				return;
-			}
-		}
-
-		//	...
-		list($path, $ext) = explode('.', $path);
-
-		//	...
-		$path = ConvertPath($path);
-
-		//	...
-		OP\UNIT\WebPack::Set($ext, $path);
+		$this->Unit('WebPack')->Auto($path);
 	}
 
 	/** Get/Set title.
 	 *
-	 * @param	 string	 $title
-	 * @param	 string	 $separator
-	 * @return	 string	 $title
+	 * @param  string  $title
+	 * @param  string  $separator
+	 * @return string  $title
 	 */
-	static function Title($title=null)
+	function Title($title=null, $separator=' | ')
 	{
-		static $_titles, $separator=' | ';
+		//	...
+		static $_title = [];
+
+		//	...
 		if( $title ){
-			$_titles[] = $title;
+			//	Stack title.
+			array_unshift($_title, $title.$separator);
+		}else{
+			//	Return title.
+			return join('', $_title) . (Env::Get('app')['title'] ?? null);
+		};
+	}
+
+	/** Unique User ID.
+	 *
+	 * @return  string  $uuid
+	 */
+	function UUID()
+	{
+		//	...
+		if(!$uuid = Cookie::Get('uuid') ){
+			$uuid = substr( md5($_SERVER['REMOTE_ADDR'] . microtime()), 0, 10);
+			Cookie::Set('uuid', $uuid);
 		}
-		return join($separator, array_reverse($_titles));
+
+		//	...
+		return $uuid;
+	}
+
+	/** Convert to url from meta url.
+	 *
+	 * @param   string  $path
+	 * @return  string  $url
+	 */
+	function URL(string $url)
+	{
+		//	...
+		static $_app, $_locale;
+
+		//	Cache
+		if( $_app === null ){
+			$_app = ConvertURL('app:/');
+		};
+
+		//	Cache
+		if( $_locale === null ){
+			//	...
+			if( Env::Get('app')['g11n'] ?? null ){
+				$_locale = Cookie::Get('locale') ?? '';
+			}else{
+				$_locale = false;
+			};
+		};
+
+		//	Check if url query.
+		if( $pos = strpos($url, '?')){
+			$que = substr($url, $pos);
+			$url = substr($url, 0, $pos);
+			$url = rtrim($url,'/').'/';
+		};
+
+		//	Check if app root.
+		$result = ($url === 'app:/') ? $_app: ConvertURL($url);
+
+		//	...
+		if( $_locale ){
+
+			//	...
+			$path = substr($result, strlen($_app)-1);
+
+			//	...
+			$result = $_app . $_locale . $path;
+		};
+
+		//	...
+		return '/'.ltrim($result,'/') . ($que ?? null);
+	}
+
+	/** CDN FQDN
+	 *
+	 * @created  2019-04-18
+	 * @param    string      $url
+	 * @return   string      $url
+	 */
+	function CDN(string $url)
+	{
+		//	...
+		$url = $this->URL($url);
+
+		//	...
+		if( Env::isLocalhost() ){
+			return $url;
+		};
+
+		//	...
+		if(!Env::Get('cdn')['execute'] ?? null ){
+			return $url;
+		};
+
+		//	...
+		$scheme = empty($_SERVER['HTTPS']) ? 'http':'https';
+		$domain = Env::Get('cdn')['domain'];
+
+		//	...
+		return "{$scheme}://{$domain}{$url}";
+	}
+
+	/** Canonical
+	 *
+	 * @created  2019-04-17
+	 * @param    string     $url
+	 * @return   string     $fqdn
+	 */
+	function Canonical($url=null)
+	{
+		//	...
+		$config = Env::Get('canonical');
+
+		//	...
+		$scheme = $config['scheme'] ?? empty($_SERVER['HTTPS']) ? 'http':'https';
+		$domain = $config['domain'] ?? $_SERVER['HTTP_HOST'];
+		$uri    = $url              ?? $_SERVER['REQUEST_URI'];
+
+		//	...
+		return "{$scheme}://{$domain}{$uri}";
 	}
 }
