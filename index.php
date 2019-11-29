8388676d2651892ac0a812b2719b057666a236db
Tomoaki imac5k-2017 <tomoaki.nagahara@gmail.com>
2019-11-29 14:10:32 +0900

    Fix: index.php - 2020

--- index.php
+++ index.php
@@ -2,12 +2,25 @@
 /**
  * unit-app:/index.php
  *
- * @creation  2018-04-04
+ * @created   2018-04-04
  * @version   1.0
  * @package   unit-app
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
+
+/** namespace
+ *
+ * @created   2019-11-21
+ */
+namespace OP;
+
+/** Load
+ *
+ */
+Unit::Load('Router');
+Unit::Load('Layout');
+Unit::Load('Template');
 
 /** Include
  *
